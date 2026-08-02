01e6b2b21d3e237177a9ba5878146e621c459df5
Jules François <45234989+phorcys-jules@users.noreply.github.com>
2026-08-02 13:18:39 +0200

    Set v0.5.0-alpha version

--- scripts/core/game_version.gd
+++ scripts/core/game_version.gd
@@ -1,8 +1,8 @@
 class_name GameVersion
 extends RefCounted
 
-const VALUE := "v0.3.0-alpha"
-const SAVE_VERSION := 1
+const VALUE := "v0.5.0-alpha"
+const SAVE_VERSION := 3
 
 static func display_name() -> String:
     return "Dungeon Architect %s" % VALUE
