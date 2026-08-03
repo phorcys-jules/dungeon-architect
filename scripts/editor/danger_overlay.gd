521e8a5a966170d4b9d99c9e24006e2531438c66
Jules François <45234989+phorcys-jules@users.noreply.github.com>
2026-08-03 23:09:24 +0200

    Modernize danger overlay for Godot 4

--- scripts/editor/danger_overlay.gd
+++ scripts/editor/danger_overlay.gd
@@ -1,17 +1,15 @@
 class_name DangerOverlay
 extends Node2D
 
-# Draws a translucent overlay on danger cells (array of Vector2i in 'cells')
-
-export(Array, Vector2i) var cells: Array = []
-export(Color) var color: Color = Color(1, 0.2, 0.2, 0.35)
-export(int) var cell_size := 48
+@export var cells: Array[Vector2i] = []
+@export var color: Color = Color(1.0, 0.2, 0.2, 0.35)
+@export var cell_size: int = 48
 
 func _draw() -> void:
-    for c in cells:
-        var pos = Vector2(c.x * cell_size, c.y * cell_size)
-        draw_rect(Rect2(pos, Vector2(cell_size, cell_size)), color)
+	for cell in cells:
+		var position := Vector2(cell.x * cell_size, cell.y * cell_size)
+		draw_rect(Rect2(position, Vector2(cell_size, cell_size)), color)
 
-func set_cells(new_cells: Array) -> void:
-    cells = new_cells
-    update()
+func set_cells(new_cells: Array[Vector2i]) -> void:
+	cells = new_cells
+	queue_redraw()
