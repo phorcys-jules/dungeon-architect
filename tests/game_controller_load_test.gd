9ea54a6c2f5144e669335b253dcdf3dbff9ce067
Jules François <45234989+phorcys-jules@users.noreply.github.com>
2026-08-04 04:29:06 +0200

    Expose game controller runtime load error

--- tests/game_controller_load_test.gd
+++ tests/game_controller_load_test.gd
@@ -1,11 +1,14 @@
 extends SceneTree
 
-const GameControllerScript := preload("res://scripts/game_controller.gd")
-
 func _init() -> void:
+    var controller_script := ResourceLoader.load("res://scripts/game_controller.gd", "Script", ResourceLoader.CACHE_MODE_IGNORE)
+    if controller_script == null:
+        push_error("game_controller.gd could not be loaded")
+        quit(1)
+        return
     var controller := Node2D.new()
-    controller.set_script(GameControllerScript)
-    assert(controller.get_script() == GameControllerScript)
+    controller.set_script(controller_script)
+    assert(controller.get_script() == controller_script)
     controller.free()
     print("game controller load test passed")
     quit(0)
